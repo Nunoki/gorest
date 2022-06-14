0072616ff940a405362c7aa751f1bdcf87b0fe57
Nino <nino@turcin.me>
2022-06-15 00:39:01 +0200

    Add DEPRECATED to code tags

--- scripts/code-tags.sh
+++ scripts/code-tags.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Finds all occurrences of the conventional tags such as TODO, FIXME, etc, in
 # all files in the project, excluding the vendor directory, and self.
-find . -type f \( -name "*.md" -or -name "*.txt" -or -name "*.go" -or -name "*.sh" \) -not -path './vendor/*' -not -path ${BASH_SOURCE} | xargs grep -n -E 'TODO|FIXME|DOCME|BUG|XXX|HACK'
+find . -type f \( -name "*.md" -or -name "*.txt" -or -name "*.go" -or -name "*.sh" \) -not -path './vendor/*' -not -path ${BASH_SOURCE} | xargs grep -n -E 'TODO|FIXME|DOCME|BUG|XXX|HACK|DEPRECATED'
