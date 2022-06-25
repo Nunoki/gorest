b2d737dada1f998c568aac9a70c6e453d72e6bb5
Nino <nino@turcin.me>
2022-06-25 20:19:54 +0200

    Change helptext helper script to only accept --help or -h arguments

--- scripts/_help_text.sh
+++ scripts/_help_text.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# Helper script that outputs the content of the $HELPTEXT variable if a help flag was passed. It
-# will accept anything that begins with "h", so --help or -h will both work (along with other 
-# variants)
-while getopts ":h" option; do
-	case $option in
-		h) # display Help
-			echo $HELPTEXT
-			exit;;
-	esac
-done
+# Helper script that outputs the content of the $HELPTEXT variable if the (first) argument is either
+# `--help` or `-h`
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]
+then
+	echo $HELPTEXT
+	exit 0
+fi
