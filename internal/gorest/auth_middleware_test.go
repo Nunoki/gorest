b0fe8b68db41123595112fdd110ebbde78b39233
Nino <nino@turcin.me>
2022-09-07 09:47:27 +0200

    Fix dummy auth tests

--- internal/gorest/auth_middleware_test.go
+++ internal/gorest/auth_middleware_test.go
@@ -3,14 +3,13 @@ package gorest
 import (
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
 
 func TestDummyAuthOk(t *testing.T) {
-	ch := make(chan struct{})
-
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
 	r.Use(AuthMiddleware())
@@ -19,52 +18,37 @@ func TestDummyAuthOk(t *testing.T) {
 		c.String(http.StatusOK, "ok")
 	})
 
-	go func(ch chan struct{}) {
-		ch <- struct{}{}
-		r.Run(":4200")
-	}(ch)
-
-	<-ch
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:4200", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
 	req.Header.Set("Authorization", "Bearer debug")
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
+	r.ServeHTTP(resp, req)
+
+	res := resp.Result()
+	if res.StatusCode != 200 {
+		t.Fatalf("expected code 200, received %d", res.StatusCode)
 	}
-	if resp.StatusCode != 200 {
-		t.Fatalf("expected code 200, received %d", resp.StatusCode)
-	}
-	content, _ := io.ReadAll(resp.Body)
+	content, _ := io.ReadAll(res.Body)
 	if string(content) != "ok" {
 		t.Fatalf("expected body to equal \"ok\", received %s", string(content))
 	}
 }
 
 func TestDummyAuthForbidden(t *testing.T) {
-	ch := make(chan struct{})
 	gin.SetMode(gin.TestMode)
-	r := gin.Default()
+	r := gin.New()
 	r.Use(AuthMiddleware())
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "ok")
 	})
 
-	go func(ch chan struct{}) {
-		ch <- struct{}{}
-		r.Run(":4200")
-	}(ch)
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	r.ServeHTTP(resp, req)
 
-	<-ch
-	resp, err := http.Get("http://localhost:4200")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp.StatusCode != 401 {
-		t.Fatalf("expected code 401, received %d", resp.StatusCode)
+	res := resp.Result()
+
+	if res.StatusCode != 401 {
+		t.Fatalf("expected code 401, received %d", res.StatusCode)
 	}
 }
