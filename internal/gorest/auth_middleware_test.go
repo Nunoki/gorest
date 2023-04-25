ae446fcd0344eca7a2901ce4e361c9921d6f01bf
Nino <nino@turcin.me>
2023-04-25 06:58:31 +0200

    Update auth middleware test

--- internal/gorest/auth_middleware_test.go
+++ internal/gorest/auth_middleware_test.go
@@ -1,54 +1,54 @@
 package gorest
 
-// import (
-// 	"io"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+)
 
-// func TestDummyAuthOk(t *testing.T) {
-// 	gin.SetMode(gin.TestMode)
-// 	r := gin.New()
-// 	r.Use(AuthMiddleware())
+func TestDummyAuthMiddlewareOK(t *testing.T) {
+	r := chi.NewRouter()
+	r.Use(dummyAuthMiddleware())
 
-// 	r.GET("/", func(c *gin.Context) {
-// 		c.String(http.StatusOK, "ok")
-// 	})
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render.Status(r, http.StatusOK)
+		render.PlainText(w, r, "ok")
+	})
 
-// 	resp := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/", nil)
-// 	req.Header.Set("Authorization", "Bearer debug")
-// 	r.ServeHTTP(resp, req)
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("Authorization", "Bearer debug")
+	r.ServeHTTP(resp, req)
 
-// 	res := resp.Result()
-// 	if res.StatusCode != 200 {
-// 		t.Fatalf("expected code 200, received %d", res.StatusCode)
-// 	}
-// 	content, _ := io.ReadAll(res.Body)
-// 	if string(content) != "ok" {
-// 		t.Fatalf("expected body to equal \"ok\", received %s", string(content))
-// 	}
-// }
+	res := resp.Result()
+	if res.StatusCode != 200 {
+		t.Fatalf("expected code 200, received %d", res.StatusCode)
+	}
+	content, _ := io.ReadAll(res.Body)
+	if string(content) != "ok" {
+		t.Fatalf("expected body to equal \"ok\", received %s", string(content))
+	}
+}
 
-// func TestDummyAuthForbidden(t *testing.T) {
-// 	gin.SetMode(gin.TestMode)
-// 	r := gin.New()
-// 	r.Use(AuthMiddleware())
+func TestDummyAuthMiddlewareForbidden(t *testing.T) {
+	r := chi.NewRouter()
+	r.Use(dummyAuthMiddleware())
 
-// 	r.GET("/", func(c *gin.Context) {
-// 		c.String(http.StatusOK, "ok")
-// 	})
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render.Status(r, http.StatusOK)
+		render.PlainText(w, r, "ok")
+	})
 
-// 	resp := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/", nil)
-// 	r.ServeHTTP(resp, req)
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	r.ServeHTTP(resp, req)
 
-// 	res := resp.Result()
-
-// 	if res.StatusCode != 401 {
-// 		t.Fatalf("expected code 401, received %d", res.StatusCode)
-// 	}
-// }
+	res := resp.Result()
+	if res.StatusCode != 401 {
+		t.Fatalf("expected code 401, received %d", res.StatusCode)
+	}
+}
